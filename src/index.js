d071f8b9698cfab3b3810437cf43223f3114067c
parangat11 <parangataditya001@gmail.com>
2024-12-14 01:59:52 +0530

    Add the basic functionality to fetch the forecast (of next 5 days)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import "./style.css"
 
+const locationBox = document.querySelector('#location-ip');
+const submitBtn = document.querySelector('#submit');
+
+async function showTheWeather(location) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=QYRSBVZGMMFQCKJQ2F54JT4KS`;
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    console.log(result.address);
+    for(let i = 0; i < 5; i++) {
+        const currDateDiv = document.querySelector(`.day-${i+1}`);
+        currDateDiv.textContent = result.days[i+1].conditions;
+    }
+}
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const location = locationBox.value;
+    showTheWeather(location);
+})
