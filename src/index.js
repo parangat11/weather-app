986ec720f8cd85fe60f3c7a1a6ef8086e34cd46f
parangat11 <parangataditya001@gmail.com>
2024-12-14 02:13:53 +0530

    Add loader functionality

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import "./style.css"
 
 const locationBox = document.querySelector('#location-ip');
 const submitBtn = document.querySelector('#submit');
+const loader = document.querySelector('#loader');
+
+function showLoader() {
+    loader.classList.add('visible');
+}
+
+function hideLoader() {
+    loader.classList.remove('visible');
+}
+
+function clearPreviousResponses() {
+    for(let i = 0; i < 5; i++) {
+        const currDateDiv = document.querySelector(`.day-${i+1}`);
+        currDateDiv.textContent = "";
+    }
+}
 
 async function showTheWeather(location) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=QYRSBVZGMMFQCKJQ2F54JT4KS`;
+    clearPreviousResponses();
+    showLoader();
     const response = await fetch(url);
     const result = await response.json();
+    hideLoader();
     console.log(result);
     console.log(result.address);
     for(let i = 0; i < 5; i++) {
